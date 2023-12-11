4e334aa441ffc402112d6baac0dc0b784fa7f803
June <godan6869@naver.com>
2023-12-12 00:49:10 +0900

    :bug: 빌드 버그 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,40 @@
 // import { redirect } from "next/navigation";
 
 import { useRouter } from "next/navigation";
-import Home from "@/app/page";
+import styles from "@/app/page.module.css";
+import { imgUrls } from "@/data";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect } from "react";
 
 export default function LoginPage() {
   const router = useRouter();
-  router.replace("/auth/sign-in", { scroll: false });
+
+  useEffect(() => {
+    router.replace("/auth/sign-in", { scroll: false });
+  }, [router]);
+
   // redirect("/auth/sign-in");
 
-  return <Home />;
+  return (
+    <div className={styles.gallery}>
+      {imgUrls.map((imgUrl, index) => {
+        return (
+          <Link key={index} href={`/photo/${index}`} scroll={false}>
+            <Image
+              src={imgUrl}
+              alt={imgUrl}
+              width={100}
+              height={100}
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              sizes="60vw"
+              priority
+            />
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 // redirect 는 서버측에서 동작하기 때문에 인터셉트가 제대로 동작하지않는다.
